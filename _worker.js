b9ca41b38fb6fe25bdbfcf971b41e276e42eadc2
MinitorMHS <99525672+MinitorMHS@users.noreply.github.com>
2023-09-01 11:16:03 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
    async fetch(request, env) { 
      let url = new URL(request.url); 
      if (url.pathname.startsWith('/')) { 
-       url.hostname = 'browserleaks.com' 
+       url.hostname = 'broad-tidal-authority.glitch.me' 
        let new_request = new Request(url, request); 
        return fetch(new_request); 
      } 
